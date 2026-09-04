3053c7efebf01f1b8e3de5e11df92e1d525d5292
Andreas <ameistad@gmail.com>
2026-09-04 22:49:46 +0200

    test: verify relative time formatting

--- internal/helpers/date_test.go
+++ internal/helpers/date_test.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/oklog/ulid"
@@ -152,22 +153,44 @@ func TestFormatDateStringWithLocation_BasicCases(t *testing.T) {
 	}
 }
 
-func TestFormatTime_Basic(t *testing.T) {
-	// Test with a time in the past
-	pastTime := time.Now().Add(-1 * time.Hour)
-	result := FormatTime(pastTime)
+func TestFormatTime(t *testing.T) {
+	const day = 24 * time.Hour
+	tests := []struct {
+		elapsed time.Duration
+		want    string
+	}{
+		{0, "1 second ago"},
+		{time.Second, "1 second ago"},
+		{2 * time.Second, "2 seconds ago"},
+		{time.Minute - time.Nanosecond, "59 seconds ago"},
+		{time.Minute, "1 minute ago"},
+		{2 * time.Minute, "2 minutes ago"},
+		{time.Hour - time.Nanosecond, "59 minutes ago"},
+		{time.Hour, "1 hour ago"},
+		{2 * time.Hour, "2 hours ago"},
+		{day - time.Nanosecond, "23 hours ago"},
+		{day, "1 day ago"},
+		{2 * day, "2 days ago"},
+		{30*day - time.Second, "29 days ago"},
+		{30 * day, "1 month ago"},
+		{60 * day, "2 months ago"},
+		{365*day - time.Second, "12 months ago"},
+		{365 * day, "1 year ago"},
+		{2 * 365 * day, "2 years ago"},
+		{-time.Second, "1 second from now"},
+		{-2 * time.Hour, "2 hours from now"},
+	}
 
-	assert.NotEmpty(t, result)
-	assert.Contains(t, result, "ago")
-}
-
-func TestFormatTimeWithLocation_Basic(t *testing.T) {
-	// Test with a time in the past
-	pastTime := time.Now().Add(-1 * time.Hour)
-	result := FormatTimeWithLocation(pastTime, time.UTC)
-
-	assert.NotEmpty(t, result)
-	assert.Contains(t, result, "ago")
+	for _, tt := range tests {
+		t.Run(tt.elapsed.String(), func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				got := FormatTime(time.Now().Add(-tt.elapsed))
+				if got != tt.want {
+					t.Fatalf("FormatTime() = %q, want %q", got, tt.want)
+				}
+			})
+		})
+	}
 }
 
 // Simple entropy source for testing
